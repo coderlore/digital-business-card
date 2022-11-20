4d677f3dc200848d1e094c674720f315e42b307f
coderlore <coderlore@gmail.com>
2022-11-20 13:46:56 -1000

    Styled footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,18 @@
 import React from "react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSquareTwitter } from '@fortawesome/free-brands-svg-icons'
+import { faSquareFacebook } from '@fortawesome/free-brands-svg-icons'
+import { faSquareInstagram } from '@fortawesome/free-brands-svg-icons'
+import { faSquareGithub } from '@fortawesome/free-brands-svg-icons'
 
 export default function Footer() {
     return (
-        <div>
+        <div className="footer">
             <ul>
-                <li>Twitter</li>
-                <li>FB</li>
-                <li>IG</li>
-                <li>GitHub</li>
+                <li className="footer-icons"><FontAwesomeIcon icon={faSquareTwitter} /></li>
+                <li className="footer-icons"><FontAwesomeIcon icon={faSquareFacebook} /></li>
+                <li className="footer-icons"><FontAwesomeIcon icon={faSquareInstagram} /></li>
+                <li className="footer-icons"><FontAwesomeIcon icon={faSquareGithub} /></li>
             </ul>
         </div>
     )
